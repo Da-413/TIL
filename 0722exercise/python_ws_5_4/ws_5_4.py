19b0c9cda4694e1b1703d195d5a43ecef5418de6
이연규 <gyoo413@naver.com>
2024-07-22 15:30:56 +0900

    0722 commit2

--- 0722exercise/python_ws_5_4/ws_5_4.py
+++ 0722exercise/python_ws_5_4/ws_5_4.py
@@ -1,6 +1,12 @@
 # 아래 함수를 수정하시오.
 def capitalize_words(string):
-    return string.title()
+    
+    title_str = []
+
+    for s in string.split(' '):
+        title_str.append(s.capitalize())
+
+    return ' '.join(title_str)
 
 result = capitalize_words("hello, world!")
 print(result)
